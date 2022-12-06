aba8e0297cea2362ecf38e6f895ebf0f332855ec
rahimsib22 <rahimsovetov@gmail.com>
2022-12-07 04:02:04 +0600

    создал тк создал переменнную для того чтобы определять приложение запущенно или нет создал событие на нажатие крестика установил чтобы нельзя было менять размер экрана установил размер экрана бесконечный цикл в котором проверяю что приложение работает

--- KRESTIKI.py
+++ KRESTIKI.py
@@ -1,2 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+import time
+import random
+
+
+tk = Tk()
+app_running = True
+
+def on_clothing():
+    global app_running
+    if messagebox.askokcancel("Quit", "do you wont to quit?"):
+        app_running = False
+        tk.destroy()
+
+tk.protocol("WM_DELETE_WINDOW", on_clothing)
+tk.title("Крестики нолики от Рахима(пожалуста можно не летник")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+canvas = Canvas(tk, width=768, height=768, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+while 1:
+    if app_running:
+        tk.update_idletasks()
+        tk.update()
+    time.sleep(0.005)
